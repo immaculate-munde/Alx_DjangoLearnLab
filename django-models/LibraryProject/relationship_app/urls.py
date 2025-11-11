e596cf98d674965f75cdaba46798c4e1e439944e
Immaculate Munde <immaculatemunde1@gmail.com>
2025-11-11 16:34:25 +0300

    Include  url patterns to route to the newly created routes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .views import LibraryDetailView
+from .views import list_books
 
 urlpatterns = [
     path('', views.home, name='home'),  # Root URL
